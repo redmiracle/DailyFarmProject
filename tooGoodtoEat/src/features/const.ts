732be4cf7af211393643e36c972b57225c0ab7a9
Maxim <besstrakhov.m@gmail.com>
2025-02-19 13:40:39 +0200

    add redux to render,cards

---
+++ tooGoodtoEat/src/features/const.ts
@@ -0,0 +1,42 @@
+import {cadsType, saleCardsType} from "../types";
+
+export const recCards:cadsType[]= [
+    {
+        name: "name 1",
+        description: "description1",
+        src: "https://c.pxhere.com/photos/1a/bc/red_food_vegetables_tomato_healthy_ketchup_tomatoes_fresh-456925.jpg!d"
+    },
+    {
+        name: "name 2",
+        description: "description2",
+        src: "https://get.pxhere.com/photo/food-produce-vegetable-market-carrot-vegetables-carrots-vegetable-garden-1058100.jpg"
+    },
+
+]
+
+
+export const saleCards:saleCardsType[]= [
+    {
+        name: "sale name 1",
+        description: "description sale 1",
+        src: "https://img.goodfon.ru/wallpaper/nbig/c/48/badfon-autumn-harvest-still-life-1849.webp",
+        saleValue:"50%"
+    },
+    {
+        name: "sale name2",
+        description: "description2",
+        src: "https://img.goodfon.ru/wallpaper/nbig/a/99/autumn-harvest-pumpkin-still-7681.webp",
+        saleValue:"20%"
+    },
+
+]
+
+export const favoriteCards:cadsType[]= [
+    {
+        name: "name 1",
+        description: "description1",
+        src: "https://c.pxhere.com/photos/1a/bc/red_food_vegetables_tomato_healthy_ketchup_tomatoes_fresh-456925.jpg!d"
+    },
+
+]
+
